6a26414e18ecba39ad4210e80a58e36a34ff8f16
Rakesh Kundu <159798591+rax-2@users.noreply.github.com>
2025-01-02 01:25:32 +0530

    Disable Github Calender
    GitHub Calendar have some COR5 issue, calendar not loading on client browser

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -9,14 +9,14 @@ export default function Projects() {
   
   return (
     <div className="PrjBody p-2 ">
-      <div className='m-2'>
+      {/*  <div className='m-2'>
         <h2 className='font-bold text-[18px]'>I manage multiple GitHub accounts. Below is an overview of my GitHub activity</h2>
         <div className='flex flex-wrap p-2 justify-center items-center '>
           <GitHubCalendarComponent username={"pin4ka"} />
           <GitHubCalendarComponent username={"rax-2"} />
         </div>
         <p className='text-[10px] text-center font-semibold'>I manage multiple GitHub accounts to deepen my understanding of GitHub workflows, including pull requests, pushes, commits, and version control coordination between accounts. </p>
-      </div>
+      </div> */}
       <div className='m-2'>
         <p className='font-bold text-[18px]'>The Complete Collection of My Projects </p>
         <div className='flex m-2 p-2 flex-wrap justify-center '>
